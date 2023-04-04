c050ea05ec7634583ce61ded46a91d8d6e0110ff
DiceglieA <angelodiceglie10@gmail.com>
2023-04-05 00:53:08 +0200

    Creata la classe calculator e implementate le funzioni

--- net-calculator-tester/Calculator.cs
+++ net-calculator-tester/Calculator.cs
@@ -12,5 +12,6 @@ namespace net_calculator_tester
         public static float Subtract(float a, float b) { return a - b; }
         public static float Multiply(float a, float b) { return a * b; }
         public static float Divide(float a, float b) { return a / b; }
+
     }
 }
